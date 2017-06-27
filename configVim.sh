0923c8978074718051c92ea24249af89ad74ae1a
Reinaldo Astudillo <astudillor@users.noreply.github.com>
2017-06-27 13:42:17 +0200

    create directory for persistent undo

--- configVim.sh
+++ configVim.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir -p ~/.vim
+mkdir -p ~/.vim/undodir
 git clone -b with_vundle https://github.com/astudillor/vimrc.git ~/.vim/vimrc
 ln -s ~/.vim/vimrc/vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
